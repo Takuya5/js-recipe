f327c4e5f511b3129086405c26ce00a462ffdeb2
takuyaabe <takuya.abe.0916@gmail.com>
2021-05-02 15:56:37 +0900

    カウントを復習した

--- 1_workspace/1/3の倍数/main.js
+++ 1_workspace/1/3の倍数/main.js
@@ -1,11 +1,7 @@
 const genkiFunction = function(number) {
   for (let n = 1; n <= number; n++) {
-    if (n % 15 === 0) {
-      console.log(`FizzBuzz`)
-    } else if (n % 3 === 0) {
-      console.log("Fizz")
-    } else if (n % 5 === 0) {
-      console.log("Buzz")
+    if (n % 3 === 0) {
+      console.log(n + "!!!!!")
     } else {
       console.log(n)
     }
--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,24 +1,7 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
-const minusButton = document.getElementById("minus-button")
-const nibaiButton = document.getElementById("nibai-button")
-
 let count = 0
-
-// ボタンをクリックしたときの処理を登録
 plusButton.onclick = function() {
-  // count を更新
   count += 1
-  // count を表示
-  display.textContent = count
-}
-
-minusButton.onclick = function() {
-  count -= 1
-  display.textContent = count
-}
-
-nibaiButton.onclick = function() {
-  count *= 2
   display.textContent = count
 }
