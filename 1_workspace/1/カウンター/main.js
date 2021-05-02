e07ef3189173aee6f12ddf811ae162bf5d3d0b19
takuyaabe <takuya.abe.0916@gmail.com>
2021-05-02 16:11:00 +0900

    カウントにボタンを付け加えた

--- 1_workspace/1/カウンター/main.js
+++ 1_workspace/1/カウンター/main.js
@@ -1,7 +1,17 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusbutton = document.getElementById("minus-button")
+const kakeruButton = document.getElementById("kakeru-button")
 let count = 0
 plusButton.onclick = function() {
   count += 1
+  display.textContent = count
+}
+minusbutton.onclick = function() {
+  count -= 1
+  display.textContent = count
+}
+kakeruButton.onclick = function() {
+  count *= 2
   display.textContent = count
 }
