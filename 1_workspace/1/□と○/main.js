1d61b96714d862b23c46c3bb6112703ecb2a9566
takuyaabe <takuya.abe.0916@gmail.com>
2021-05-02 22:58:07 +0900

    □と△を復習した

--- 1_workspace/1/□と○/main.js
+++ 1_workspace/1/□と○/main.js
@@ -1,11 +1,5 @@
 const figure = document.getElementById("figure")
 
 figure.onclick = function() {
-  figure.classList.add("rounded")
-  if (figure.classList.contains("rounded")) {
-    figure.classList.remove("rounded")
-  }
-}
-figure.onmouseover = function() {
-  figure.classList.add("rounded")
+  figure.classList.toggle("rounded")
 }
