b38dee5f507087e7569f532054470f145156a7c4
takuyaabe <takuya.abe.0916@gmail.com>
2021-05-06 18:57:14 +0900

    自販機をやった

--- 1_workspace/1/ストップウォッチ/main.js
+++ 1_workspace/1/ストップウォッチ/main.js
@@ -7,16 +7,7 @@ const countUp = function() {
   count += 1
   display.textContent = count / 100
 }
-
-let id = null
-
 button.onclick = function() {
-  if (id === null) {
-    id = setInterval(countUp, 10)
-    button.textContent = "stop"
-  } else {
-    clearInterval(id)
-    id = null
-    button.textContent = "start"
-  }
+  setInterval(countUp, 10)
+  button.textContent = "stop"
 }
